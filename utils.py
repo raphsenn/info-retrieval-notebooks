7d0644c4e24dab63cb488c19f6bb19a3a07238c0
raphsenn <raphaelsenn@outlook.de>
2024-08-08 14:55:35 +0200

    inverted index

---
+++ utils.py
@@ -0,0 +1,13 @@
+import re
+
+WORD_PATTERN = '[a-zA-Z]+'
+
+
+def get_words(text: str) -> list[str]:
+    """
+    >>> text = 'You are... awesome!'
+    >>> words = get_words(text)
+    >>> words
+    ['you', 'are', 'awesome']
+    """
+    return re.findall(WORD_PATTERN, text.lower())
