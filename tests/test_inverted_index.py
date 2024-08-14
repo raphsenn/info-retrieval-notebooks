120e5550ee17d04bb42aa99d547c42c7d381f309
raphsenn <raphaelsenn@outlook.de>
2024-08-14 10:35:05 +0200

    clean code

---
+++ tests/test_inverted_index.py
@@ -0,0 +1,8 @@
+from ..src.inverted_index import InvertedIndex
+
+def test_intersect():
+    ii = InvertedIndex()
+    arr1 = [2, 4, 6, 8, 100]
+    arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 100, 200]
+    res = ii.intersect(arr1, arr2)
+    assert res == [2, 4, 6, 8, 100]
